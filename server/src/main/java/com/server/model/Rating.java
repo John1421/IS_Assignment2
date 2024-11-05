8f8bc90aa53b35f1c6f9ef7eeee66b587bdaae02
Zan Pizmoht <42890960+zanp98@users.noreply.github.com>
2024-11-05 14:56:32 +0000

    Add rating model and repository

---
+++ server/src/main/java/com/server/model/Rating.java
@@ -0,0 +1,20 @@
+package com.server.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("ratings")
+public class Rating {
+    @Id
+    private long userId;
+    private long mediaId;
+    private double rating;
+    private String userName;
+}
---
+++ server/src/main/java/com/server/repository/RatingRepository.java
@@ -0,0 +1,12 @@
+package com.server.repository;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import com.server.model.Rating;
+
+import reactor.core.publisher.Flux;
+
+public interface RatingRepository extends ReactiveCrudRepository<Rating, Long> {
+    Flux<Rating> findByMediaId(Long mediaId);
+    Flux<Rating> findByUserId(Long userId);
+}
