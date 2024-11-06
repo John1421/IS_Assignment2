154c7f7a23aec0b2676ab59835cb4a8bee5b22cd
João Samuel Diogo <joaosamueldiogo@gmail.com>
2024-11-06 17:59:00 +0000

    Rating to relationship

---
+++ server/src/main/java/com/server/model/Relationship.java
@@ -0,0 +1,18 @@
+package com.server.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("relationships")
+public class Relationship {
+    @Id
+    private long userId;
+    private long mediaId;
+}
